6a6e2ae5b2ce6a66aea1eac8deb64ec6a6ba4907
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-06-22 10:00:18 +0500

    variable

---
+++ app.js
@@ -0,0 +1,16 @@
+//   002          variable
+// means containers number, string etc
+// let a= "Fatima"                  //literal
+// console.log(a);                  //daclaring variable
+// //  java script case sensitive hoti h
+// Q1 : write a function 'checkEvenOdd' that accepts a number and prints whether it is even or odd?
+// solution:
+function checkEvenOdd(num) {
+    if (num % 2 == 0) {
+        console.log("It's an even number.");
+    }
+    else {
+        console.log("It's an odd number.");
+    }
+}
+checkEvenOdd(11);
---
+++ app.ts
@@ -0,0 +1,9 @@
+//   002          variable
+
+// means containers number, string etc
+
+
+// let a= "Fatima"                  //literal
+// console.log(a);                  //daclaring variable
+// //  java script case sensitive hoti h
+
